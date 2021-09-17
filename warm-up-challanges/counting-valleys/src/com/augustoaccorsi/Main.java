04b99605d2b6af74d390f089cdba9d6f5f0f5e7e
augustoaccorsi <augusto.acorsi@gmail.com>
2021-09-17 16:20:54 -0300

    add counrinf valleys

---
+++ warm-up-challanges/counting-valleys/src/com/augustoaccorsi/Main.java
@@ -0,0 +1,12 @@
+package com.augustoaccorsi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(countingValleys(8, "UDDDUDUU"));
+    }
+
+    public static int countingValleys(int steps, String path) {
+        return steps;
+    }
+}
