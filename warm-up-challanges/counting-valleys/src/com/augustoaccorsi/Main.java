73eb031a7de5d86ef203cbc20c48830e252f0944
augustoaccorsi <augusto.acorsi@gmail.com>
2021-09-17 16:48:06 -0300

    add response

--- warm-up-challanges/counting-valleys/src/com/augustoaccorsi/Main.java
+++ warm-up-challanges/counting-valleys/src/com/augustoaccorsi/Main.java
@@ -3,10 +3,32 @@ package com.augustoaccorsi;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(countingValleys(8, "UDDDUDUU"));
+        System.out.println(countingValleys(12, "DDUUDDUDUUUD"));
     }
 
     public static int countingValleys(int steps, String path) {
-        return steps;
+
+        int seaLevel = 0, valley = 0;
+        boolean flag = false;
+
+        for(char c: path.toCharArray()){
+            if(c == 'U'){
+                seaLevel++;
+            }
+            else{
+                seaLevel--;
+            }
+
+            if(seaLevel < 0){
+                flag = true;
+            }
+
+            if(flag && seaLevel >= 0){
+                valley++;
+                flag = false;
+            }
+
+        }
+        return valley;
     }
 }
