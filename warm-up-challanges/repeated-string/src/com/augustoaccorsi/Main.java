45bbbad271e25553de6baa9f12049288127c2b1e
augustoaccorsi <augusto.acorsi@gmail.com>
2021-09-18 12:45:54 -0300

    add my solution for repeated strings

--- warm-up-challanges/repeated-string/src/com/augustoaccorsi/Main.java
+++ warm-up-challanges/repeated-string/src/com/augustoaccorsi/Main.java
@@ -3,6 +3,32 @@ package com.augustoaccorsi;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String string = "kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm";
+        long number = Long.parseLong("736778906400");
+
+        System.out.println(repeatedString(string, number));
+    }
+
+    public static long repeatedString(String string, long number) {
+        long size = string.length(), repeated = number/size;
+        long left = number - (size * repeated);
+        int extra = 0;
+
+        int count = 0;
+        for(char c: string.toCharArray()){
+            if(c == 'a'){
+                count++;
+            }
+        }
+
+        for(int i = 0; i < left; i++){
+            if(string.charAt(i) == 'a'){
+                extra++;
+            }
+        }
+
+        repeated = (repeated * count) + extra;
+
+        return repeated;
     }
 }
