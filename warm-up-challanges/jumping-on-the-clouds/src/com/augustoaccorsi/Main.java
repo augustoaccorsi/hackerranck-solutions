83fab31f2a693a83730a062818c1d7d5052ba7ab
augustoaccorsi <augusto.acorsi@gmail.com>
2021-09-18 10:12:30 -0300

    add my solution for jumping clouds

--- warm-up-challanges/jumping-on-the-clouds/src/com/augustoaccorsi/Main.java
+++ warm-up-challanges/jumping-on-the-clouds/src/com/augustoaccorsi/Main.java
@@ -1,8 +1,53 @@
 package com.augustoaccorsi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> clouds = new ArrayList<>();
+        clouds.add(0);
+        clouds.add(0);
+        clouds.add(1);
+        clouds.add(0);
+        clouds.add(0);
+        clouds.add(1);
+        clouds.add(0);
+        System.out.println(jumpingOnClouds(clouds));
+    }
+
+    public static int jumpingOnClouds(List<Integer> clouds) {
+        int jumps = 0, index =0;
+        boolean flag = false;
+        do{
+            try {
+                if (clouds.get(index) == 0) {
+                    if (clouds.get(index + 2) == 0) {
+                        index += 2;
+                        jumps++;
+                    } else if (clouds.get(index + 1) == 0) {
+                        index += 1;
+                        jumps++;
+                    }
+                }
+            }
+            catch (Exception e){
+                try {
+                    if (clouds.get(index + 1) == 0) {
+                        index += 1;
+                        jumps++;
+                    }
+                }
+                catch (Exception ex){ flag = true;}
+            }
+
+            if(index >= clouds.size()){
+                flag = true;
+            }
+        }
+        while (!flag);
+
+        return jumps;
     }
 }
